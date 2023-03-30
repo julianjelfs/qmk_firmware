e611408131a0758928f5f25adc58dc93f0461e98
Julian Jelfs <julian.jelfs@gmail.com>
2023-03-30 22:18:19 +0100

    pinky tweaks

--- keyboards/pinky/3/keymaps/jelfs/keymap.c
+++ keyboards/pinky/3/keymaps/jelfs/keymap.c
@@ -37,12 +37,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_3x7_4(
         KC_TAB         , KC_Q   , KC_W        , KC_E       , KC_R        , KC_T          , _______            ,       _______  , KC_Y        , KC_U        , KC_I       , KC_O   , KC_P   , KC_EQL ,
         KC_ESC         , KC_A   , LT(2, KC_S) , LT(1, KC_D), LSFT_T(KC_F), LT(3, KC_G)   , KC_VOLD            ,       KC_VOLU  , LT(3, KC_H) , RSFT_T(KC_J), LT(1, KC_K), LT(2, KC_L)   , KC_SCLN, KC_QUOT,
-        LCTL_T(KC_GRV) , KC_Z   , KC_X        , KC_C       , KC_V        , KC_B          , TD(TD_NOTHING_CAPS),       KC_MPLY  , KC_N        , KC_M        , KC_COMM    , KC_DOT , KC_SLSH, KC_NUHS,
-                                                KC_LALT    , KC_LGUI     , KC_BSPC, KC_DEL                    ,       KC_ENT   , KC_SPC      , KC_RCTL     , KC_NUBS
+        LCTL_T(KC_NUBS) , KC_Z   , KC_X        , KC_C       , KC_V        , KC_B          , TD(TD_NOTHING_CAPS),       KC_MPLY  , KC_N        , KC_M        , KC_COMM    , KC_DOT , KC_SLSH, KC_NUHS,
+                                                KC_LALT    , KC_LGUI     , KC_BSPC, KC_DEL                    ,       KC_ENT   , KC_SPC      , KC_RCTL     , KC_GRV
     ),
     [_ONE] = LAYOUT_split_3x7_4(
-        LCTL(KC_NUBS), KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_MINS, _______,    _______, _______, KC_7, KC_8   , KC_9, _______, _______,
-        LGUI(KC_NUBS), _______, _______, KC_LBRC, KC_RBRC, KC_UNDS, _______,    _______, _______, KC_4, KC_5   , KC_6, _______, _______,
+        LCTL(KC_GRV), KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_MINS, _______,    _______, _______, KC_7, KC_8   , KC_9, _______, _______,
+        LGUI(KC_GRV), _______, _______, KC_LBRC, KC_RBRC, KC_UNDS, _______,    _______, _______, KC_4, KC_5   , KC_6, _______, _______,
         _______     , _______, _______, KC_LPRN, KC_RPRN, _______, _______,    _______, _______, KC_1, KC_2   , KC_3, _______, _______,
                                         _______, _______, _______, _______,    _______, _______, KC_0, _______
     ),
